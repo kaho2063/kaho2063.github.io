c0005ad427257b08437a84186a9dd39f8fc335cd
kaho2063 <kaho2063@gmail.com>
2021-06-16 13:32:03 +0900

    ・テンプレートからモジュールを使った形に変更

--- getID.js
+++ getID.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     afterText.value = text
       .replace(/１/g, '1').replace(/２/g, '2').replace(/３/g, '3')
       .replace(/\(コスト\d\)/g, '')
-      .replace(/[ 　・：]*(\d+)[ 　:：×]+([^枚\n]+)枚?\n*/g, '{{PopupBattle|$1|×$2}}')
-      .replace(/^[ 　・：]*(\d+)\n*/gm, '{{PopupBattle|$1}}');
+      .replace(/[ 　・：]*(\d+)[ 　:：×]+([^枚\n]+)枚?\n*/g, '{{#invoke:Popup|getBattlePage|$1|×$2}}')
+      .replace(/^[ 　・：]*(\d+)\n*/gm, '{{#invoke:Popup|getBattlePage|$1}}');
   });
   const changePassiveText = document.getElementById('changePassiveText');
   changePassiveText.addEventListener('click', () => {
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     afterText.value = text
       .replace(/[（(][^)）]+[)）]/g, '')
       .replace(/[：:][0-9０-９]?コスト[0-9０-９]?/g, '')
-      .replace(/・?([^、\n・]+)、?\n*/g, '{{PopupPassive|$1}}');
+      .replace(/・?([^、\n・]+)、?\n*/g, '{{#invoke:Popup|getPassive|$1}}');
   });
   const changeDeckType = document.getElementById('changeDeckType');
   changeDeckType.addEventListener('click', () => {
